0527846bbcf352ca746eae53105ae1db11b85d8f
DtP Mario <mariodieck@gmx.de>
2023-03-25 06:09:23 +0100

    Version: 1.0.0.4
    descVersion erhöt (angepasst auf Gameversion 1.9.0.0)
    Klee (Clover) konnte mit dem PickUp zwar aufgenommen werden aber aus dem Rohr (Häcksler) kam kein Partikelsystem
    		(Fehler war in der fruitTypes.xml <fruitTypeConverters> stand COLVER_WINDROW statt CLOVER_WINDROW)
    Pflanzendichte bei Lavendel und Luzerne runtergesetzt (FPS drops)
    Weißkohl war nicht mehr sichtbar in der letzten Erntestufe
    Huds der zusätzlichen Früchte ausgetauscht
    deko Büsche können nun gemäht werden
    weiter bodenlayer hinzugefügt so das man inGame mehrere auswahl hat zum Malen
    pickupEffects.xml angepasst beim aufnehmen von Grasschwad mit dem PickUp kam es zu Fehlern

--- scripts/SpeedCamera.lua
+++ scripts/SpeedCamera.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:810c4a2e79ae994a03ae36bf931e22236374dd6195d99698d8c2efabdd8c8bfa
-size 4891
+oid sha256:d88b715b3c9f46a475e5253c3cb9fb801d998aeb129a7d36855e5c7735cf3733
+size 4887
