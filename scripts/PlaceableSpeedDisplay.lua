49a087d8ee903afa492c2953e1d73cf6c4dc6fa2
DtP Mario <mariodieck@gmx.de>
2023-04-02 21:52:50 +0200

    Beta 12
    einige Verkehrsschilder schrifftlich angepasst
    viele Verkehrsschilder hatten keine Colision
    Straße am Dorf angepasst da es diverse Lücken gab
    SpeedDisplay im Dorf funktional gemacht (zeigt an wie schnell man fährt
    neue Felder definiert
    	  Felder
    		24
    		25
    		26
    		27
    		28
    		29
    		30
    		31
    		32
    		33
    /Ampeln verbauen

---
+++ scripts/PlaceableSpeedDisplay.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c923a1162af4689fc3367a69364e897faf519cca990a72fdc0c64eee46e26c7
+size 11566
---
+++ scripts/TrafficLightCrossing.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:314e5cf9aa6fec94c991dfb72b7910e7528047c7ce9c50689aeeb2a1095fa1a5
+size 11575
